960aae87532e3f258832973e908cd994fba65a0e
Daveram-web <daveram2273@gmail.com>
2025-01-02 12:15:22 +0530

    Input-Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import Input from 'antd/es/input/Input';
+import React from 'react';
+import { Input } from 'antd';
+import { UserAddOutlined } from '@ant-design/icons';
 import './App.css';
-import {UserAddOutlined} from '@ant-design/icons';
+
+const { Search } = Input;
 
 function App() {
-
+  const onSearch = (value) => {
+    console.log('Search text:', value);
+  };
 
   return (
     <div className="App">
-     <header className='App-header'>
-      <Input
-      placeholder=' Name'
-      prefix = {<UserAddOutlined />}
-      allowClear
-      
-      >
-      </Input>
-     </header>
+      <header className="App-header">
+        <Input
+          placeholder="Name"
+          prefix={<UserAddOutlined />}
+          maxLength={10}
+          allowClear
+          style={{ marginBottom: 20 }} // Adds spacing between components
+        />
+
+        <Search
+          placeholder="Input search text"
+          enterButton="Search"
+          size="large"
+          onSearch={onSearch} 
+          />
+      </header>
     </div>
   );
 }
