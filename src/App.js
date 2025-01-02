fea8039b87b274083bfe509ab35e2a58276c674a
Daveram-web <daveram2273@gmail.com>
2025-01-02 12:34:59 +0530

    Select-Componet

--- src/App.js
+++ src/App.js
@@ -1,76 +1,29 @@
-import './App.css';
-import {  Table } from 'antd';
+import { Select } from 'antd'
+import React from 'react'
 
 function App() {
-const data =[{
-  name:'Name 1',
-  age:21,
-  address :'Adress 1',
-  key : '1'
-},
-{name:'Name 2',
-  age:22,
-  address :'Adress 2',
-  key : '2'
-},
-{
-  name:'Name 3',
-  age:16,
-  address :'Adress 3',
-  key : '3'
-
-},
-{
-  name:'Name 3',
-  age:25,
-  address :'Adress 3',
-  key : '4'
-
-}]
-
-const columns = [{
-  title : () =>    <p style ={{color : 'red'}}>Name</p>,
-  dataIndex : 'name',
-  key : 'name',
-  sorter : (a,b) => a.name.localeCompare(b.name), 
-  render : name =>{
-    return <a href="#">{name}</a>
-  },
-},
-{
-  title : 'Age',
-  dataIndex : 'age',
-  key : 'age',
-  sorter :(a,b) =>a.age - b.age
-},
-{
-  title : 'address',
-  dataIndex : 'address',
-  key : 'address',
-},
-{
- title : 'Adult',
- key : 'key',
- render : adult =>{
- return <p>{adult.age > 18 ? "Adult" : "Not-Adult"} </p>
-}
-}
-
-]
-
+  const bike = ['Mountain Bike', 'Road Bike', 'Hybrid Bike', 'Electric Bike'];
 
   return (
     <div className="App">
-     <header className='App-header'>
-      <Table
-      columns={columns}
-      dataSource={data}
+      <header className="App-header">
+        <p>Select based on the option:</p>
+        <Select
+          mode="multiple"
+          maxCount={3}
+          allowClear
+          placeholder="Select bike types"
+          style={{ width: '400px', marginBottom: '20px' }}
         >
-
-      </Table>
-     </header>
+          {bike.map((bike, index) => (
+            <Select.Option key={index} value={bike}>
+              {bike}
+            </Select.Option>
+          ))}
+        </Select>
+      </header>
     </div>
   );
 }
 
-export default App;
+export default App
