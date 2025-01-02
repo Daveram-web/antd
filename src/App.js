2e9ddf2040028a80cadc9bc2a858d1e376ed9d78
Daveram-web <daveram2273@gmail.com>
2025-01-02 11:29:13 +0530

    Tabel-Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,74 @@
-import { useState } from 'react';
 import './App.css';
-import { Button } from 'antd';
-import { RightOutlined } from '@ant-design/icons'
+import {  Table } from 'antd';
 
 function App() {
-const [loading, setLoading] = useState(false)
-const handelClick = () => {
- setLoading(true)
- setTimeout(() => {
- setLoading(false)
-  }, 2000)}
+const data =[{
+  name:'Name 1',
+  age:21,
+  address :'Adress 1',
+  key : '1'
+},
+{name:'Name 2',
+  age:22,
+  address :'Adress 2',
+  key : '2'
+},
+{
+  name:'Name 3',
+  age:16,
+  address :'Adress 3',
+  key : '3'
+
+},
+{
+  name:'Name 3',
+  age:25,
+  address :'Adress 3',
+  key : '4'
+
+}]
+
+const columns = [{
+  title : () =>    <p style ={{color : 'red'}}>Name</p>,
+  dataIndex : 'name',
+  key : 'name',
+  sorter : (a,b) => a.name.localeCompare(b.name), 
+  render : name =>{
+    return <a href="#">{name}</a>
+  },
+},
+{
+  title : 'Age',
+  dataIndex : 'age',
+  key : 'age',
+  sorter :(a,b) =>a.age - b.age
+},
+{
+  title : 'address',
+  dataIndex : 'address',
+  key : 'address',
+},
+{
+ title : 'Adult',
+ key : 'key',
+ render : adult =>{
+ return <p>{adult.age > 18 ? "Adult" : "Not-Adult"} </p>
+}
+}
+
+]
+
 
   return (
     <div className="App">
-      <header className='App-header'>
-      <Button
-        type='primary'
-        block
-        icon = {<RightOutlined />}
-        className='my-button'
-        onClick={handelClick}
-        loading={loading}
-      >
-        loagind
-      </Button>
-      </header>
+     <header className='App-header'>
+      <Table
+      columns={columns}
+      dataSource={data}
+        >
+
+      </Table>
+     </header>
     </div>
   );
 }
