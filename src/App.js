c6422e512c6773d4396c150ef0b49126b6473c58
Daveram-web <daveram2273@gmail.com>
2025-01-03 16:18:55 +0530

    Menu-Component

--- src/App.js
+++ src/App.js
@@ -1,74 +1,40 @@
+import { Input, Menu, Space } from 'antd';
 import './App.css';
-import {  Table } from 'antd';
+import React from 'react';
+import { UserOutlined ,HomeOutlined,DashboardOutlined,DollarOutlined,UserSwitchOutlined,UserDeleteOutlined  } from '@ant-design/icons';
 
 function App() {
-const data =[{
-  name:'Name 1',
-  age:21,
-  address :'Adress 1',
-  key : '1'
-},
-{name:'Name 2',
-  age:22,
-  address :'Adress 2',
-  key : '2'
-},
-{
-  name:'Name 3',
-  age:16,
-  address :'Adress 3',
-  key : '3'
-
-},
-{
-  name:'Name 3',
-  age:25,
-  address :'Adress 3',
-  key : '4'
-
-}]
-
-const columns = [{
-  title : () =>    <p style ={{color : 'red'}}>Name</p>,
-  dataIndex : 'name',
-  key : 'name',
-  sorter : (a,b) => a.name.localeCompare(b.name), 
-  render : name =>{
-    return <a href="#">{name}</a>
-  },
-},
-{
-  title : 'Age',
-  dataIndex : 'age',
-  key : 'age',
-  sorter :(a,b) =>a.age - b.age
-},
-{
-  title : 'address',
-  dataIndex : 'address',
-  key : 'address',
-},
-{
- title : 'Adult',
- key : 'key',
- render : adult =>{
- return <p>{adult.age > 18 ? "Adult" : "Not-Adult"} </p>
-}
-}
-
-]
-
 
   return (
-    <div className="App">
-     <header className='App-header'>
-      <Table
-      columns={columns}
-      dataSource={data}
-        >
-
-      </Table>
-     </header>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}>
+      <Space>
+      <Menu
+      mode='inline'
+      onClick={(info)=>{
+        console.log(info.key);
+        
+      }}
+      defaultOpenKeys={["dashboard"]}
+      items={[
+        {label : <Input.Search placeholder='Search .....' ></Input.Search>,key:"search" },
+        {label :"Home ", key :"home",icon :<HomeOutlined />},
+        {label:"DashBoard", key:"dashboard" , icon:<DashboardOutlined /> ,children:[
+        {label:"Revenu" ,key :"rev",icon:<DollarOutlined />},
+        {label :"Expence" , key :"exp"}]},
+        {label :"User Managemnt", key :"us", icon:<UserOutlined />, children:[
+          {label:"Sign out" ,key :"so",icon:<UserDeleteOutlined />},
+          {label :"Switch account" , key :"sa",icon:<UserSwitchOutlined />}]},
+        {label:"Sign-out",key :"signout" , danger:true,icon:<UserOutlined />}
+      
+      ]}
+      >
+      </Menu>
+      </Space>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Pagination from './Pagination';
-import Selection from './Selection';
-import CRUD from './CRUD';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Selection /> */}
-    <CRUD />
+    <App />
   </React.StrictMode>
 );
 
